8685cc723af8a76d5d85bd8c34a852e8c9912c69
Mayank Patel <mayankpatelmehta@gmail.com>
2026-05-22 11:17:37 +0530

    chore: bump api_version to 2.3

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from bson.objectid import ObjectId
 import os
+
 import sys
 import torch
 import torch.nn as nn
